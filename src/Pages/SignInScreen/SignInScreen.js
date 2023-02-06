129150908adbf20a0c4478ced4d070dee69af0fe
haristunner <haristunner10@gmail.com>
2023-02-06 17:43:17 +0530

    firebase auth setup

--- src/Pages/SignInScreen/SignInScreen.js
+++ src/Pages/SignInScreen/SignInScreen.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SignInScreen.css"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from '../../firebase'
 
 export const SignInScreen = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const register = (e) => {
+        e.preventDefault()
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((authUser) => {
+                console.log(authUser);
+            })
+            .catch((err) => {
+                alert(err.message)
+            })
+
+    }
+
+    const signIn = (e) => {
+        e.preventDefault()
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((signInUser) => {
+                console.log(signInUser)
+            })
+            .catch((err) => {
+                alert(err.message)
+            })
+    }
+
     return (
         <div className='signInScreen'>
             <form>
                 <h1>Sign In</h1>
-                <input type="email" placeholder='Email' />
-                <input type="password" placeholder='Password' />
-                <button type='submit'>Sign In</button>
+                <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+                <button type='submit' onClick={signIn}>Sign In</button>
                 <h4>
                     <span className='signInScreen-gray'>New to Netflix?</span>
-                    <span className='signInScreen-link'>Sign Up Now</span>
+                    <span className='signInScreen-link' onClick={register}>Sign Up Now</span>
                 </h4>
             </form>
         </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBfR04lQgMo1kZk7IHp_FMsJcZseW6PHsg",
     authDomain: "netflix-clone-9e433.firebaseapp.com",
@@ -7,3 +17,11 @@ const firebaseConfig = {
     appId: "1:466322067667:web:25be47d1e3863bece4c426",
     measurementId: "G-TSX8C1GBBP"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore()
+
+export { app, auth, db }
